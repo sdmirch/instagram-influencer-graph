109da6276d0078d0f2f13eba3042760d38da2bbf
Sera Mirchandani <seramirchandani@Seras-MBP.domain>
2017-10-18 13:28:56 -0700

    add code to make selena gomez fake graph for testing

--- graph_util/make_graph.py
+++ graph_util/make_graph.py
@@ -9,7 +9,7 @@ def make_graph(filepath, directed=True):
     Create a graph using data stored in followers collection from instacrape db.
 
     Args:
-        filepath (str): Filepath where graph will be save (.gml format)
+        filepath (str): Filepath where graph will be saved (.gml format)
         directed (Optional[bool]): Creates directed graph by default.
 
     Returns: None
@@ -28,3 +28,30 @@ def make_graph(filepath, directed=True):
             G.add_edge(follower['node']['id'], influencer)
 
     nx.write_gml(G, filepath)
+
+def make_fake_graph(G, filepath, num_followers=181507):
+    """
+    Create a graph for testing purposes with a macro-influencer ("SelenaGomez").
+
+    Args:
+        G (nx.Graph()): Graph object, base graph of influencers and followers.
+        filepath (str): Filepath where graph will be saved (.gml format)
+        num_followers [Optional(int)]: Number of followers to be added from current
+            follower base and to be added as fake users. Total added is 2*num_followers.
+
+    Returns: None
+    """
+    G_fake = G.copy()
+
+    G_fake.add_node("SelenaGomez")
+
+    # Add n=181507 followers from follower base
+    follower_choices = set(G.nodes()).difference(set(influencers.keys()))
+    selena_followers = np.random.choice(list(follower_choices), size=181507, replace=False)
+
+    # Add n=181507 fake followers who are not part of the follower base (not in community)
+    for i in range(181507):
+        random_generated_user = "Fake" + str(i)
+        G_fake.add_edge(random_generated_user, "SelenaGomez")
+
+    nx.write_gml(G_fake, filepath)
