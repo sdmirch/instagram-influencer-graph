cab57824957200ae2adbbbf46dee275ab74a668a
Sera Mirchandani <sdmirch@email.arizona.edu>
2017-10-25 13:09:44 -0700

    add dictionary functionality to pipeline

--- src/influencer_pipeline.py
+++ src/influencer_pipeline.py
@@ -24,13 +24,14 @@ def reduce_by_critical_mass(G, crit_mass=5000):
             G_reduced.remove_node(node)
     return G_reduced
 
-def calc_community_influence_score(G):
+def calc_community_influence_score(G,returned='list'):
     """
     Calculate and sort by community influence scores, which is eigenvector centrality
-        for a graph.
+        for a graph (higher scores for higher eigenvector centralities).
 
     Args:
         G (nx.Graph()): Graph object.
+        returned [Optional(list)]: 'list' or 'dict' will change data structure returned.
 
     Returns:
         sorted_influence_score (list of tuples): List of nodes sorted by eigenvector
@@ -38,18 +39,21 @@ def calc_community_influence_score(G):
     """
 
     c = nx.eigenvector_centrality(G)
-    sorted_influence_score = (sorted(c.iteritems(), key=lambda x: x[1], reverse=True))
-    return sorted_influence_score
+    if returned == 'list':
+        sorted_influence_score = (sorted(c.iteritems(), key=lambda x: x[1], reverse=True))
+        return sorted_influence_score
+    else:
+        return c
 
-
-def calc_interaction_score(G, d):
+def calc_interaction_score(G, d, returned='list'):
     """
     Calculate and sort by interaction scores for reduced graph based
-        on likes and followers.
+        on likes and followers (higher scores for larger likes to followers ratios).
 
     Args:
         G (nx.Graph()): Graph object.
         d (dict): Dictionary with user ids, number of likes, and number of followers.
+        returned [Optional(list)]: 'list' or 'dict' will change data structure returned.
 
     Returns:
         sorted_interaction_score
@@ -63,17 +67,21 @@ def calc_interaction_score(G, d):
         else:
             interaction_score[user_id] = (1.0*d[user_id]['max_likes']/d[user_id]['num_follow'])
 
-    sorted_interaction_score = (sorted(interaction_score.iteritems(), key=lambda x: x[1], reverse=True))
-    return sorted_interaction_score
+    if returned == 'list':
+        sorted_interaction_score = (sorted(interaction_score.iteritems(), key=lambda x: x[1], reverse=True))
+        return sorted_interaction_score
+    else:
+        return interaction_score
 
-def calc_authenticity_score(G, captions):
+def calc_authenticity_score(G, captions, returned='list'):
     """
     Calculate and sort by authenticity scores for reduced graph based
-        on compound sentiment analysis score.
+        on compound sentiment analysis score (higher scores for more negative posts).
 
     Args:
         G (nx.Graph()): Graph object.
         captions (dict): Dictionary with user ids and captions.
+        returned [Optional(list)]: 'list' or 'dict' will change data structure returned.
 
     Returns:
         sorted_authenticity_score
@@ -96,6 +104,11 @@ def calc_authenticity_score(G, captions):
         df = pd.DataFrame(caption_sentiment[user_id])
         # Subtract compound score from 1 to penalize more positive captions
         caption_sentiment_means[user_id] = 1 - dict(df.mean())['compound']
+    # Add Selena Gomez for test
+    caption_sentiment_means["SelenaGomez"] = 0.5
 
-    sorted_authenticity_score = (sorted(caption_sentiment_means.iteritems(), key=lambda x: x[1], reverse=True))
-    return sorted_authenticity_score
+    if returned == 'list':
+        sorted_authenticity_score = (sorted(caption_sentiment_means.iteritems(), key=lambda x: x[1], reverse=True))
+        return sorted_authenticity_score
+    else:
+        return caption_sentiment_means
